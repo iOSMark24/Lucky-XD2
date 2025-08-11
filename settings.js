fba275dd9434bc80822810c218335ec03a3e8207
iOSMark24 <sodeindesamson76@gmail.com>
2025-08-11 22:51:39 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "lucky~7NlW0YZY#Ey415WqFkRBOaj1vEM4uoyLU2Rb9J1QOF14Kobir3Xk",
 // add your Session Id make sure it starts with lucky~
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
